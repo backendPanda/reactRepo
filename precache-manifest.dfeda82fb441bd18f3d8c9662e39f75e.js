9241c1b4e4c6b2d8156fdae9ff98da652c61c603
unknown <selassiedj@gmail.com>
2020-08-12 08:38:18 +0000

    Updates

---
+++ precache-manifest.dfeda82fb441bd18f3d8c9662e39f75e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52cf088d0edc59373616a10a6e22dbe7",
+    "url": "/backendPanda/reactRepo/index.html"
+  },
+  {
+    "revision": "00e2d541f2d27c535d90",
+    "url": "/backendPanda/reactRepo/static/css/main.aafba902.chunk.css"
+  },
+  {
+    "revision": "e6859ed0e5f7c670f602",
+    "url": "/backendPanda/reactRepo/static/js/2.c4d9963d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/backendPanda/reactRepo/static/js/2.c4d9963d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00e2d541f2d27c535d90",
+    "url": "/backendPanda/reactRepo/static/js/main.57a23db5.chunk.js"
+  },
+  {
+    "revision": "79e88d726aaefe2550f2",
+    "url": "/backendPanda/reactRepo/static/js/runtime-main.bfcc14fa.js"
+  },
+  {
+    "revision": "8da08b2c26ed9469eb50a13b7a9f2af0",
+    "url": "/backendPanda/reactRepo/static/media/flower-3140492_1920.8da08b2c.jpg"
+  },
+  {
+    "revision": "09e3970223f93dd0db261395b25ad4b5",
+    "url": "/backendPanda/reactRepo/static/media/tree-736885_1280.09e39702.jpg"
+  }
+]);
